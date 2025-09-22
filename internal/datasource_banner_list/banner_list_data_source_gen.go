a43b1bd00d8547d73be8bcbcb35123a89a0e77fa
hellt <dodin.roman@gmail.com>
2025-09-22 23:29:44 +0200

    Sync 20250922-232944

--- internal/datasource_banner_list/banner_list_data_source_gen.go
+++ internal/datasource_banner_list/banner_list_data_source_gen.go
@@ -130,7 +130,7 @@ func BannerListDataSourceSchema(ctx context.Context) schema.Schema {
 			"kind": schema.StringAttribute{
 				Computed: true,
 			},
-			"labelselector": schema.StringAttribute{
+			"label_selector": schema.StringAttribute{
 				Optional:            true,
 				Computed:            true,
 				Description:         "a label selector string to filter the results based on CR labels",
@@ -151,7 +151,7 @@ type BannerListModel struct {
 	Filter        types.String `tfsdk:"filter"`
 	Items         types.List   `tfsdk:"items"`
 	Kind          types.String `tfsdk:"kind"`
-	Labelselector types.String `tfsdk:"labelselector"`
+	LabelSelector types.String `tfsdk:"label_selector"`
 	Namespace     types.String `tfsdk:"namespace"`
 }
 
